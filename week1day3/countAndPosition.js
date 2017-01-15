defec9882d3a66478b4cac862615664a3b956772
EdwardXyWang <edwardxywang@gmail.com>
2017-01-15 18:46:52 +0000

    Use nested object to finish it, pretty cool

--- week1day3/countAndPosition.js
+++ week1day3/countAndPosition.js
@@ -1,42 +1,22 @@
 var inputLetters = process.argv.slice(2).join('').toLowerCase().split('');
 
-var result = {
+function countAndPosition(inputLetters) {
+  var result = {
 
-};
-
-var numberList = [];
-for (var i = 0; i < inputLetters.length; i++) {
-  numberList.push(i);
-}
-// console.log(numberList);
-
-while (numberList.length !== 0) {
-  numberListToModify = numberList.slice();
-  result[inputLetters[numberList[0]]] = {
-    appearNumber: 0,
-    appearPosition: ''
   };
-  for (var i in numberList) {
-    if (inputLetters[numberList[0]] === inputLetters[i]) {
-      result[inputLetters[numberList[0]]]['appearNumber'] ++;
-      result[inputLetters[numberList[0]]]['appearPosition'] += (', ' + i);
-      // console.log(i);
-      numberListToModify.splice(i - (numberList.length - numberListToModify.length), 1);
+
+  for (var i = 0; i < inputLetters.length; i++) {
+    if(!result.hasOwnProperty(inputLetters[i])) {
+      result[inputLetters[i]] = {};
+      result[inputLetters[i]]['frequency'] = 1;
+      result[inputLetters[i]]['position'] = String(i);
+    } else {
+      result[inputLetters[i]]['frequency'] ++;
+      result[inputLetters[i]]['position'] += ', ' + i;
     }
-    // debugger;
-  }
-numberList = numberListToModify;
-// console.log(numberList);
-}
-console.log(result);
-// for (var i = 0; i < inputLetters.length; i++) {
-//   for (var j = i; j < inputLetters.length; j++) {
-//     if (!result.hasOwnProperty(inputLetters[j])) {
-//       result[inputLetters[i]].count = 1;
-//       result[inputLetters[i]].position = i;
-//     } else {
-//       result[inputLetters[i]].count ++;
-//       result[inputLetters[i]].position += (', ' + j);
-//     }
-//   }
-// }
+  };
+  return result;
+ }
+
+console.log(countAndPosition(inputLetters));
+
